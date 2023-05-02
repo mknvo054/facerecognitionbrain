c546304f4ee0a0cfe9097a7493a6dffe1eca4ca0
mknvo054 <50524580+mknvo054@users.noreply.github.com>
2023-05-02 06:34:56 -0600

    check input from user

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import ParticlesBg from "particles-bg";
+import { useState } from "react";
 import "./App.css";
 import "tachyons";
 
 function App() {
+  function onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
       <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm InputChange={onInputChange} />
       {/*<FaceRecognition /> */}
     </div>
   );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,6 @@
 import "./ImageLinkForm.css";
 
-function ImageLinkForm() {
+function ImageLinkForm({ InputChange }) {
   return (
     <div>
       <p className="f3">
@@ -8,7 +8,11 @@ function ImageLinkForm() {
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="text" />
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            onChange={InputChange}
+          />
           <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
             Detect
           </button>
